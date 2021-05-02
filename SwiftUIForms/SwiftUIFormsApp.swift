0d666ecc5e62a53961f34eb3300687dddf8eaf96
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-05-02 00:25:56 -0300

    Initial Commit

---
+++ SwiftUIForms/SwiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormsApp.swift
+//  SwiftUIForms
+//
+//  Created by Adrian De León on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
