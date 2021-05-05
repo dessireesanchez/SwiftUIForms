d828f291523081225203c47de022198da58b9752
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-05-05 00:47:19 -0300

    Add ViewModel for input data

--- SwiftUIForms/ContentView.swift
+++ SwiftUIForms/ContentView.swift
@@ -7,37 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class FormViewModel: ObservableObject {
     @State var firstName = ""
     @State var lastName = ""
     @State var password = ""
     @State var passwordAgain = ""
+}
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = FormViewModel()
+   
     var body: some View {
         NavigationView {
             VStack {
                 Form {
                     Section {
-                        TextField("First Name", text: $firstName)
-                        TextField("Last Name", text: $lastName)
+                        TextField("First Name", text: $viewModel.firstName)
+                        TextField("Last Name", text: $viewModel.lastName)
                     }
                     
                     Section(footer: Text("Your password must be at least 8 characters long.")) {
-                        SecureField("Create Password", text: $password)
-                        SecureField("Confirm Password", text: $passwordAgain)
+                        
+                        SecureField("Create Password", text: $viewModel.password)
+                        SecureField("Confirm Password", text: $viewModel.passwordAgain)
                     }
-                
-                    Section {
-                        Button(action: {
-                            
-                        }, label: {
-                            Text("Continue")
-                                .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(8)
-                        })
+                    
+                    Section(header: Text("Mailing Address")) {
+                        TextField("Street Address", text: $viewModel.firstName)
+                        TextField("City", text: $viewModel.firstName)
+                        TextField("State", text: $viewModel.firstName)
+                        TextField("Postal Code", text: $viewModel.firstName)
+                        TextField("Country", text: $viewModel.firstName)
+
                     }
                 }
+                
+                Divider()
+                
+                Button(action: {
+                    // Do something with form data
+                }, label: {
+                    Text("Continue")
+                        .frame(width: 250, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+                .padding()
             }
             .navigationTitle("Create Account")
         }
@@ -47,6 +64,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
     }
 }
